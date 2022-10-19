ee928f72777af6cee2ccfaa0f248bb724d3ea259
AL SABAB <sabab015@gmail.com>
2022-10-19 16:57:01 +0600

    sql injection added

--- functions.php
+++ functions.php
@@ -5,7 +5,10 @@
         if (isset($_POST['submit'])) {
             $username = $_POST['username'];
             $password = $_POST['password'];
-        
+            
+            $username= mysqli_real_escape_string($connection, $username);
+            $password= mysqli_real_escape_string($connection, $password);
+
             $query = "INSERT INTO users(username,password) ";
             $query .= "VALUES('$username','$password')";
             $result = mysqli_query($connection, $query);
